5648503e91c3368bebf7e2be3b6c453406e2414f
Steve Boyer <stephen.a.boyer@gmail.com>
2021-09-08 14:41:26 -0500

    setup directory structure, added middleware for JSON data

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-// ..
-app.get('/', (req, res) => res.send('Hello world'));
-app.listen(port, () => console.log('Application started at => http://localhost:'+port));
+app.use(express.json({
+  extended: false
+}));
+app.use(express.urlencoded({
+  extended: false
+}));
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => {
+  res.render('home');
+});
+app.listen(port, () => console.log('Application started at => http://localhost:' + port));
