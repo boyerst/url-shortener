170cc958d440f133f6f6c1a18befad7e8f8bcc25
Steve Boyer <stephen.a.boyer@gmail.com>
2021-09-14 11:49:22 -0500

    mongodb cluster created, db connected

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+
+const connectDB = require('./config/db.js')
+connectDB();
+
 app.use(express.json({
   extended: false
 }));
